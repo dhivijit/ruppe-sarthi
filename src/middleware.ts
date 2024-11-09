72d86dc40975ce0509a8bb5839ab71a040a12d48
Dhivijit Koppuravuri <68179868+dhivijit@users.noreply.github.com>
2024-11-09 22:56:04 +0530

    Middleware update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,18 @@
 import { auth } from "@/auth"
  
 export default auth((req) => {
-//   if (!req.auth && req.nextUrl.pathname !== "/auth/login") {
-//     const newUrl = new URL("/auth/login", req.nextUrl.origin)
-//     return Response.redirect(newUrl)
-//   }
 
   if(!req.auth && req.nextUrl.pathname == "/"){
+    const newUrl = new URL("/auth/login?error=You need to log in", req.nextUrl.origin)
+    return Response.redirect(newUrl)
+  }
+
+  if(!req.auth && req.nextUrl.pathname == "/finance"){
+    const newUrl = new URL("/auth/login?error=You need to log in", req.nextUrl.origin)
+    return Response.redirect(newUrl)
+  }
+
+  if(!req.auth && req.nextUrl.pathname == "/settings"){
     const newUrl = new URL("/auth/login?error=You need to log in", req.nextUrl.origin)
     return Response.redirect(newUrl)
   }
@@ -16,7 +22,7 @@ export default auth((req) => {
     return Response.redirect(newUrl)
   }
 
-  if(req.auth && req.nextUrl.pathname == "/signup"){
+  if(req.auth && req.nextUrl.pathname == "/auth/signup"){
     const newUrl = new URL("/", req.nextUrl.origin)
     return Response.redirect(newUrl)
   }
